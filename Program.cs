a8c823f28122ad65762267d8b4552b8b2c3d9170
Denis <denis-boz@hotmail.com>
2023-06-17 14:06:01 +0300

    Решение задачи 41

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+
+int[] GetArray(int size)
+{
+    int[] newArray = new int[size];
+    for (int i = 0; i < newArray.Length; i++)
+    {
+        Console.Write($"Введите элемент массива под индексом {i}: ");
+        newArray[i] = int.Parse(Console.ReadLine());
+    }
+    return newArray;
+}
+int[] resultArray = GetArray(5);
+Console.WriteLine($"Введенный массив: [{String.Join(", ", resultArray)}]");
+
+int result = 0;
+for (int i = 0; i < resultArray.Length; i++)
+{
+    if (resultArray[i] > 0)
+    {
+        result ++;
+    }
+}
+Console.WriteLine($"Чисел больше нуля: {result}");
